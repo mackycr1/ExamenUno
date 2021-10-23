339d741139c53e748d05e93688317617e7989bf4
mackycr1 <mackycr1@gmail.com>
2021-10-22 20:02:25 -0600

    Added Inheritances to Tipo Inquilino Entity. Created Tipo Inquilino Service which allows the Web App to perform CRUD operations via the Service.

---
+++ Entity/TipoInquilinoEntity.cs
@@ -0,0 +1,9 @@
+﻿namespace Entity
+{
+    public class TipoInquilinoEntity : DBEntity
+    {
+        public int? IdTipoInquilino { get; set; }
+        public string Descripcion { get; set; }
+        public bool Estado { get; set; }
+    }
+}
---
+++ WBL/TipoInquilinoService.cs
@@ -0,0 +1,102 @@
+﻿using BD;
+using Entity;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace WBL
+{
+    public interface ITipoInquilinoService
+    {
+        Task<DBEntity> Insert(TipoInquilinoEntity entity);
+        Task<DBEntity> Update(TipoInquilinoEntity entity);
+        Task<DBEntity> Delete(TipoInquilinoEntity entity);
+        Task<IEnumerable<TipoInquilinoEntity>> Get();
+        Task<TipoInquilinoEntity> GetById(TipoInquilinoEntity entity);
+    }
+
+    public class TipoInquilinoService : ITipoInquilinoService
+    {
+        private readonly IDataAccess sql;
+
+        public TipoInquilinoService(IDataAccess sql)
+        {
+            this.sql = sql;
+        }
+
+        public async Task<IEnumerable<TipoInquilinoEntity>> Get()
+        {
+            try
+            {
+                var result = sql.QueryAsync<TipoInquilinoEntity>("TipoInquilinoObtener");
+                return await result;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public async Task<TipoInquilinoEntity> GetById(TipoInquilinoEntity entity)
+        {
+            try
+            {
+                var result = sql.QueryFirstAsync<TipoInquilinoEntity>("TipoInquilinoObtener", new { entity.IdTipoInquilino });
+                return await result;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public async Task<DBEntity> Insert(TipoInquilinoEntity entity)
+        {
+            try
+            {
+                var result = sql.ExecuteAsync("TipoInquilinoInsertar", new
+                {
+                    entity.Descripcion,
+                    entity.Estado
+                });
+                return await result;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public async Task<DBEntity> Update(TipoInquilinoEntity entity)
+        {
+            try
+            {
+                var result = sql.ExecuteAsync("TipoInquilinoActualizar", new
+                {
+                    entity.IdTipoInquilino,
+                    entity.Descripcion,
+                    entity.Estado
+                });
+                return await result;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public async Task<DBEntity> Delete(TipoInquilinoEntity entity)
+        {
+            try
+            {
+                var result = sql.ExecuteAsync("TipoInquilinoEliminar", new { entity.IdTipoInquilino });
+                return await result;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
